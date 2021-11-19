dfcdb539b245fa14a8e8a269e0ec511ea8c50b5a
diogopimenteel <diogopimenteel@gmail.com>
2021-11-19 16:50:25 -0300

    initial commit

---
+++ beerstock/src/test/java/com/dio/beerstock/beerstock/builder/BeerDTOBuilder.java
@@ -0,0 +1,41 @@
+package com.dio.beerstock.beerstock.builder;
+
+import com.dio.beerstock.beerstock.dto.BeerDTO;
+import com.dio.beerstock.beerstock.enums.BeerType;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.Builder;
+
+
+@Builder
+public class BeerDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String name = "Brahma";
+
+    @Builder.Default
+    private String brand = "Ambev";
+
+    @Builder.Default
+    private int max = 50;
+
+    @Builder.Default
+    private int quantity = 10;
+
+    @Builder.Default
+    private BeerType type = BeerType.LAGER;
+
+    public BeerDTO toBeerDTO() {
+        return new BeerDTO(id,
+                name,
+                brand,
+                max,
+                quantity,
+                type);
+    }
+}
